8515300df7e90aa35012aea89a70bb209def1ee2
grdavie <gaylereynadavie@gmail.com>
2024-02-10 15:59:27 +1000

    update json file

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,11 @@
 const baseURL = "https://grdavie.github.io/wdd230/";
+const linksURL = "https://grdavie.github.io/wdd230/data/links.json";
 
+async function getLinks () {
+
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.table(data.weeks) //for testing
+}
+
+getLinks();
